54edca9534aec35f1cfd1aaafce994935091c21d
Scott Diallo <diallot@me.com>
2016-08-31 09:01:44 -0700

    comment have been added to snippets.js for clarity

--- snippets.js
+++ snippets.js
@@ -1,6 +1,8 @@
+//Using mongoDB as data storage (nosql)
+
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
+MongoClient.connect('mongodb://localhost/snippets', function (err, db) {
     if (err) {
         console.error(err);
         db.close();
@@ -9,12 +11,12 @@ MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
 
     var collection = db.collection('snippets');
 
-    var create = function(name, content) { // creating snippet in the  database
+    var create = function (name, content) { // creating snippet in the  database
         var snippet = {
             name: name,
             content: content
         };
-        collection.insert(snippet, function(err, result){
+        collection.insert(snippet, function (err, result) {
             if (err) {
                 console.error('could not create snippet', name);
                 db.close();
@@ -27,74 +29,72 @@ MongoClient.connect('mongodb://localhost/snippets', function(err, db) {
     };
 
 
-var read = function(name) {
-    var query = {
-        name: name
-    };
-    collection.findOne(query, function(err, snippet) {
-        if (!snippet || err) {
-            console.error("Could not read snippet", name);
+    var read = function (name) {
+        var query = {
+            name: name
+        };
+        collection.findOne(query, function (err, snippet) {
+            if (!snippet || err) {
+                console.error("Could not read snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Read snippet", snippet.name);
+            console.log(snippet.content);
             db.close();
-            return;
-        }
-        console.log("Read snippet", snippet.name);
-        console.log(snippet.content);
-        db.close();
-    });
-};
+        });
+    };
 
-var update = function(name, content) {
-var query = {
-    name: name
-};
+    var update = function (name, content) {
+        var query = {
+            name: name
+        };
 
-var update = {
-    $set: {content: content} // setting the value of the field
-};
+        var update = {
+            $set: {
+                content: content
+            } // setting the value of the field
+        };
 
-collection.findAndModify(query, null, update, function(err, // method to find and update snippets
-     result) {
-    var snippet = result.value;
-    if (!snippet || err) {
-        console.error("Could not update snippet", name);
-        db.close();
-        return;
-    }
-    console.log("Updated snippet", snippet.name);
-    db.close();
-});
-};
+        collection.findAndModify(query, null, update, function (err, // method to find and update snippets
+            result) {
+            var snippet = result.value;
+            if (!snippet || err) {
+                console.error("Could not update snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Updated snippet", snippet.name);
+            db.close();
+        });
+    };
 
-// delete method to find and remove snippet
-var del = function(name, content) {
-var query = {
-    name: name
-};
-collection.findAndRemove(query, function(err, result) {
-    var snippet = result.value;
-    if (!snippet || err) {
-        console.error("Could not delete snippet", name);
-        db.close();
-        return;
-    }
-    console.log("Deleted snippet", snippet.name);
-    db.close();
-});
-};
-    var main = function() {
+    // delete method to find and remove snippet
+    var del = function (name, content) {
+        var query = {
+            name: name
+        };
+        collection.findAndRemove(query, function (err, result) {
+            var snippet = result.value;
+            if (!snippet || err) {
+                console.error("Could not delete snippet", name);
+                db.close();
+                return;
+            }
+            console.log("Deleted snippet", snippet.name);
+            db.close();
+        });
+    };
+    var main = function () {
         if (process.argv[2] == 'create') {
             create(process.argv[3], process.argv[4]);
-        }
-        else if (process.argv[2] == 'read') {
+        } else if (process.argv[2] == 'read') {
             read(process.argv[3]);
-        }
-        else if (process.argv[2] == 'update') {
+        } else if (process.argv[2] == 'update') {
             update(process.argv[3], process.argv[4]);
-        }
-        else if (process.argv[2] == 'delete') {
+        } else if (process.argv[2] == 'delete') {
             del(process.argv[3]);
-        }
-        else {
+        } else {
             console.error('Command not recognized');
             db.close();
         }
